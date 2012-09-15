b110009b9cb4fcfd7129c2bc43280cf5a57ad881
Michael Stenta <mike@mstenta.net>
2012-09-14 20:26:07 -0400

    Moved weather_snapshot and weather_video classes into separate includes. Cleaned up whitespace.

--- cron.php
+++ cron.php
@@ -6,6 +6,8 @@ require_once 'settings.php';
 
 // Load the weather classes.
 require_once 'includes/weather.inc';
+require_once 'includes/weather_snapshot.inc';
+require_once 'includes/weather_video.inc';
 
 // Set the timezone to UTC.
 date_default_timezone_set('UTC');
@@ -21,13 +23,13 @@ $weather->download();
 
 // If it's the first day of the month, generate a video.
 if (date('j') == '1') {
-  
+
   // Create a new video object.
   $video = new weather_video($weather_settings, $time);
-  
+
   // Render the video.
   $video->render();
-  
+
   // Upload the video to YouTube.
   if ($weather_settings['youtube']['upload']) {
     $video->upload();
--- settings.default.php
+++ settings.default.php
@@ -2,18 +2,18 @@
 
 // Settings array
 $weather_settings = array(
-  
+
   'debug' => FALSE,  // Debug mode (prints messages)
   'content' => '/[root]/content',  // Base directory to store downloaded/generated files in.
   'log' => '/[root]/content/log',  // Directory to store logs in
-  
+
   // Youtube upload settings.
   'youtube' => array(
     'upload' => TRUE,  // Enable/disable Youtube upload
     'email' => '',  // Youtube login email address
     'password' => '',  // Youtube login password
   ),
-  
+
   // Array of NOAA GOES image feeds to download.
   'goes_feeds' => array(
     'eaus' => array(
